9df00b0a60e46e206d6770e64d1be7b6ac76c7da
Tanmay Sinha <alokratn001@gmail.com>
2020-07-01 17:30:39 +0530

    added frontend features

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 import AppNavBar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <AppNavBar/>
+      <ShoppingList/>
     </div>
   );
 }
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -5,7 +5,6 @@ import {
     NavbarToggler,
     NavbarBrand,
     Nav,
-    NavItem,
     NavLink,
     Container,
 } from 'reactstrap';
