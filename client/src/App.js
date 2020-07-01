0cb40a06a3a8cb3bb64dd7f3aa22e6cf5acbb605
Tanmay Sinha <alokratn001@gmail.com>
2020-07-01 16:48:30 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppNavBar from './components/AppNavbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello, lets start.!</h1>
+      <AppNavBar/>
     </div>
   );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/"> Shopping List</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavLink href="https://github.com/tanmay-sinha">
+                                    Github
+                            </NavLink>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default AppNavbar;
