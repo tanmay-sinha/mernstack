abf9854aa59819ee2dbdd4dbca69a6fac40e6591
Tanmay Sinha <alokratn001@gmail.com>
2020-07-08 16:01:03 +0530

    add item feature added using redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import ItemModal from './components/ItemModal'
 import AppNavBar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
-
+import { Container } from "reactstrap";
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -14,7 +14,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavBar />
-          <ShoppingList />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     )
