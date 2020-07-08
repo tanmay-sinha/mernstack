74d1fcd489472f4919e7357649e4480cb37f92c5
Tanmay Sinha <alokratn001@gmail.com>
2020-07-08 16:15:43 +0530

    connected backend for getting items

--- client/src/actions/itemAction.js
+++ client/src/actions/itemAction.js
@@ -1,9 +1,13 @@
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
+import axios from 'axios';
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS,
-    };
+export const getItems = () => dispatch => {
+    dispatch(setItemLoading());
+    axios.get('/api/items')
+        .then(res => dispatch({
+            type: GET_ITEMS,
+            payload: res.data
+        }))
 };
 
 export const deleteItem = (id) => {
@@ -17,5 +21,11 @@ export const addItem = (item) => {
     return {
         type: ADD_ITEM,
         payload: item
+    };
+};
+
+export const setItemLoading = (item) => {
+    return {
+        type: ITEMS_LOADING
     };
 };
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,20 +1,18 @@
 import { v1 as uuid } from 'uuid';
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM} from '../actions/types';
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'milk' },
-        { id: uuid(), name: 'eggs' },
-        { id: uuid(), name: 'fish' },
-        { id: uuid(), name: 'bread' },
-    ]
+    items: [],
+    loading: false
 }
 
 export default function(state = initialState, action) {
     switch (action.type) {
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             }
         case DELETE_ITEM:
             return {
@@ -25,6 +23,11 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 items: [action.payload, ...state.items]
+            }
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             }
         default:
             return state;
