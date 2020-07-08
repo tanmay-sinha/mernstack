73da280020f8f121f84ce2a1269c40a69eca65fa
Tanmay Sinha <alokratn001@gmail.com>
2020-07-08 14:33:15 +0530

    added get items feature in redux

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { v1 as uuid } from 'uuid';
+import {connect} from 'react-redux';
+import {getItems} from '../actions/itemAction';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-    state = {
-        items: [
-            { id: uuid(), name: 'milk' },
-            { id: uuid(), name: 'eggs' },
-            { id: uuid(), name: 'fish' },
-            { id: uuid(), name: 'bread' },
-        ]
-    }
 
+    componentDidMount() {
+        this.props.getItems();
+    }
+    
     render() {
-        const { items } = this.state;
+        const { items } = this.props.item;
         return (
             <Container>
                 <Button color="dark" style={{ marginBottom: '2rem' }} onClick={() => {
@@ -54,4 +53,13 @@ class ShoppingList extends Component {
     }
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+});
+
+export default connect(mapStateToProps, { getItems })(ShoppingList);
