1b7a79c2220d8fece8c2d080a2c778cc8b50a379
Tanmay Sinha <alokratn001@gmail.com>
2020-07-10 14:22:26 +0530

    fixed a small bug in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 function auth(req, res, next) {
     const token = req.header('x-auth-token');
     
-    if (!token) res.status(401).json({ msg: 'No token, Unauthorized' });
+    if (!token) return res.status(401).json({ msg: 'No token, Unauthorized' });
 
     try {
         const decode = jwt.verify(token, config.get('jwtsecret'));
